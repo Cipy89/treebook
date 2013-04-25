56263695a6b593ce5fb338777c2ff16b1724ecee
Cipy <cipy@cipy-Aspire-6930G.(none)>
2013-04-25 17:26:11 +0200

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '5cfd97035676045d1e406ab75b534b05ceb1e349ac71cb2b7726c3067dc0c0eee9a25a24f2ec1261524fb09dcb7d3c4453114ee5574c05ee0ee7000405c16bec'
